86e45477f70004454794c2806007244bd2405ae0
ByteSizedMe <75525893+ByteSizedMe@users.noreply.github.com>
2021-01-06 18:47:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var canvas;
+var music;
+var box1,box2,box3,box4;
+var ball,edges;
+
+function preload(){
+    music = loadSound("music.mp3");
+
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+    box1 = createSprite(0,580,360,30);
+    box1.shapeColor = "blue";
+    box2 = createSprite(295,580,200,30);
+    box2.shapeColor = "red";
+    box3 = createSprite(515,580,200,30);
+    box3.shapeColor = "green";
+    box4 = createSprite(740,580,220,30);
+    box4.shapeColor = "yellow";
+    ball = createSprite(random(20,750),100,40,40);
+    ball.shapeColor = "white";
+    ball.velocityY = 5;
+    ball.velocityX = -5;
+    
+
+    //create 4 different surfaces
+
+
+
+    //create box sprite and give velocity
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    edges = createEdgeSprites();
+    ball.bounceOff(edges);
+    if(box1.isTouching(ball)&&ball.bounceOff(box1)){
+        ball.shapeColor = "blue";
+    }
+    if(box2.isTouching(ball)&&ball.bounceOff(box2)){
+        ball.shapeColor = "red";
+    }
+    if(box3.isTouching(ball)&&ball.bounceOff(box3)){
+        ball.shapeColor = "green";
+    }
+    if(box4.isTouching(ball)&&ball.bounceOff(box4)){
+        ball.shapeColor = "yellow";
+    }
+
+    drawSprites();
+    //create edgeSprite
+
+
+
+    //add condition to check if box touching surface and make it box
+}
